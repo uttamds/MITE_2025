d72f531260c63dae31e2f5ba3497a561604afe15
Uttam Shreyakar <39872039+uttamds@users.noreply.github.com>
2025-07-23 14:03:55 +0530

    Create Bank.py

---
+++ Bank.py
@@ -0,0 +1,19 @@
+class Bank_Acc:
+    def __init__(self):
+        pass
+    def add_data(self):
+        print("Enter your acc no")
+        self.accno  = input()
+        print("Enter your pin number")
+        self.__pin = input()
+    def show_pin(self):
+        print("Enter the OTP to see the pin")
+        otp = int(input())
+        if otp == 7878:
+            print("Your pin is ", slef.__pin)
+        else:
+            print("Invalid OTP")
+
+acc1 = Bank_Acc()
+acc1.add_data()
+acc1.show_pin()
